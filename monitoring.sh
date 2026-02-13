7ae5845ff2ac5302ccb996009d068e4f575dee9f
sasan <37546757+sasantech@users.noreply.github.com>
2026-02-13 15:17:18 +0330

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Self-healing: Remove Windows line endings if they exist
-if [[ $(type -P python3) ]]; then
-    export PYTHONIOENCODING=utf8
-fi
-
-# دانلود و اجرای مستقیم برای رفع مشکل CRLF
 curl -Ls https://raw.githubusercontent.com/sasantech/v2ray-monitoring/main/manager.py -o manager.py
 python3 manager.py
+exit
