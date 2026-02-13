8982e1e33e58129d6bd5c400105c3b5e8986bcf8
sasan <37546757+sasantech@users.noreply.github.com>
2026-02-13 15:10:47 +0330

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# بررسی نصب بودن پیش‌نیازها
-if ! command -v python3 &> /dev/null; then
-    echo "Installing Python3..."
-    apt update && apt install python3 python3-pip curl -y
-fi
-
 # دانلود فایل اصلی پایتون اگر وجود نداشت
 if [ ! -f "manager.py" ]; then
     echo "Downloading manager.py..."
     curl -Ls https://raw.githubusercontent.com/sasantech/v2ray-monitoring/main/manager.py -o manager.py
 fi
+
+# نصب کتابخانه مورد نیاز (اگر نصب نباشد)
+pip3 install requests &> /dev/null
 
 # اجرای فایل پایتون
 python3 manager.py
