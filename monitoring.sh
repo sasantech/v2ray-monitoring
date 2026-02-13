a7baf99885eb0533bfc5ed56985f8fd7b0cab8e8
sasan <37546757+sasantech@users.noreply.github.com>
2026-02-13 15:15:38 +0330

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if ! command -v python3 &> /dev/null; then
-    apt update && apt install python3 python3-pip curl -y
+# Self-healing: Remove Windows line endings if they exist
+if [[ $(type -P python3) ]]; then
+    export PYTHONIOENCODING=utf8
 fi
-if [ ! -f "manager.py" ]; then
-    curl -Ls https://raw.githubusercontent.com/sasantech/v2ray-monitoring/main/manager.py -o manager.py
-fi
-python3 manager.py
 
+# دانلود و اجرای مستقیم برای رفع مشکل CRLF
+curl -Ls https://raw.githubusercontent.com/sasantech/v2ray-monitoring/main/manager.py -o manager.py
+python3 manager.py
